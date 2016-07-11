e89320dd9a830fc849119990720becb053419609
Crystal <cortney0215@yahoo.com>
2016-07-11 11:00:25 -0400

    server.js now takes db credentials, connection.js completed, orm.js connecting to connection.js

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,12 +1,14 @@
-var orm = require('./config/orm.js'); 
+//var orm = require('../config/orm.js'); 
 
-module.exports = function(app){
+module.exports = function(app, orm){
 
+	// route to display the login page
 	app.get('/login', function(req, res){
 
 		
 	});
 
+	// route to post to the login page
 	app.post('/login', function(req, res){
 
 
