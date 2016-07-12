0e6744133339ad0e68cb59b83bb5c82d4017f959
Robert Prine <robertprine2@yahoo.com>
2016-07-12 14:18:41 -0400

    Added an orm call for users but my test in server.js isn't working (this needs to be removed when I get it working)

--- config/orm.js
+++ config/orm.js
@@ -7,4 +7,28 @@ module.exports = function(username, password) {
 
 	//require('./connection.js')(username, password);
 
-}
+	var orm = {
+		addUser: function(userEmail, password, user, firstN, lastN, image, userAddress, userCity, userState, userZip, phone) {
+
+			var post = {
+				email: userEmail, 
+				userPassword: password,
+				userName: user,
+				firstName: firstN,
+				lastName: lastN,
+				userImage: image,
+				address: userAddress,
+				city: userCity,
+				state: userState,
+				zip: userZip,
+				phoneNumber: phone
+			};
+
+			var query = connection.query('INSERT INTO users SET ?', post, function(err, result) {
+				console.log(result);
+			});
+			console.log(query.sql)
+		}, // end of addUser function
+	}; // end of orm object
+
+} // end of module.exports
