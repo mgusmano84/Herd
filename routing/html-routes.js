f086c05a0236fc49b6b6968576daa48833b234e1
Crystal <cortney0215@yahoo.com>
2016-07-12 19:17:21 -0400

    moved query

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -5,6 +5,7 @@ module.exports = function(app, orm){
 	// route to post to the login page
 	app.post('/login', function(req, res){
 
+		orm.addUser("robertprine2@yahoo.com", "Floop2", "BestDriverNA", "Robert", "Prine", "<img src='https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-9/13076924_1720305171578346_5638715548334228214_n.jpg?oh=64b0cf52506fe186373c3fa6901ff5ff&oe=57F78ACC'>", "8464 Lake Waverly Ln", "Orlando", "FL", 32829, "4077919189");
 
 	});
 
@@ -28,7 +29,5 @@ module.exports = function(app, orm){
 
 		res.render('home');
 	});
-
-	orm.addUser("robertprine2@yahoo.com", "Floop2", "BestDriverNA", "Robert", "Prine", "<img src='https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-9/13076924_1720305171578346_5638715548334228214_n.jpg?oh=64b0cf52506fe186373c3fa6901ff5ff&oe=57F78ACC'>", "8464 Lake Waverly Ln", "Orlando", "FL", 32829, "4077919189");
 
 }
