02cb3c01b12a338cd7efa8640c12fca8e28cf083
maryhoag <marymcevoyhoag@gmail.com>
2016-07-14 20:22:49 -0400

    added background photo

--- home.js
+++ home.js
@@ -1,12 +1,8 @@
 //seperate from the main .js file so as to not corrupt working code
 
-var source = $("#user-template").html(); 
-var template = Handlebars.compile(source); 
+var ctaBtn = $('.cta_btn');
+    header = $('header');
 
-var data = {
-	//the logged in user, so the data from the #login and #loginModal
-	//so whatever info is retrieved from that get
-};
-
-//puts the template on the page
-$('body').append(template);
+ctaBtn.hover(function(){
+  header.toggleClass('explore');
+});
