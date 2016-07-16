a7bd068fc4fdc03984cc9747f7307a0a431c7e77
Robert Prine <robertprine2@yahoo.com>
2016-07-15 20:41:25 -0400

    updated schema to have a longer password allocation

--- data/schema.sql
+++ data/schema.sql
@@ -5,7 +5,7 @@ USE carPool;
 CREATE TABLE users (
 userID INTEGER(11) AUTO_INCREMENT NOT NULL,
 email VARCHAR(50) NOT NULL,
-userPassword VARCHAR(20) NOT NULL,
+userPassword VARCHAR(1000) NOT NULL,
 userName VARCHAR(30) NOT NULL,
 firstName VARCHAR(30) NOT NULL,
 lastName VARCHAR(30) NOT NULL,
@@ -18,14 +18,14 @@ city VARCHAR(30) NOT NULL,
 -- EXTENDED**
 state VARCHAR(30) NOT NULL,
 -- EXTENDED**
-zip INTEGER(10),
+zip VARCHAR(10),
 -- EXTENDED**Could be used for drivers who need to contact parents or possibly to send quick text alerts
 phoneNumber VARCHAR(20) NOT NULL,
 PRIMARY KEY (userID)
 );
 
 -- Dummy Data for users table
-INSERT INTO users (email, userPassword, userName, firstName, lastName, userImage, address, city, state, zip, phoneNumber) VALUES ("brucewayne@notbatman.com", "RichBat", "BatmanOrBust2", "Bruce", "Wayne", "<img src='http://www.mbird.com/wp-content/uploads/2012/07/tumblr_m3b0anGHPD1qje60io1_1280.jpg'>", "3030 Goodrick Ln", "Kissimmee", "FL", 34743, "9999999999"), ("chloeLovesTreats@dog.com", "treats", "GoodDog", "Chloe", "Prine", "<img src='https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-1/p160x160/11825639_124892927852533_4183234602635820129_n.jpg?oh=2377cda37e44c8f0bf7e3af59ee6d2fd&oe=5837373F'>", "8464 Lake Waverly Ln", "Orlando", "FL", 32829, "8888888888");
+INSERT INTO users (email, userPassword, userName, firstName, lastName, userImage, address, city, state, zip, phoneNumber) VALUES ("brucewayne@notbatman.com", "RichBat", "BatmanOrBust2", "Bruce", "Wayne", "<img src='http://www.mbird.com/wp-content/uploads/2012/07/tumblr_m3b0anGHPD1qje60io1_1280.jpg'>", "3030 Goodrick Ln", "Kissimmee", "FL", "34743", "9999999999"), ("chloeLovesTreats@dog.com", "treats", "GoodDog", "Chloe", "Prine", "<img src='https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-1/p160x160/11825639_124892927852533_4183234602635820129_n.jpg?oh=2377cda37e44c8f0bf7e3af59ee6d2fd&oe=5837373F'>", "8464 Lake Waverly Ln", "Orlando", "FL", "32829", "8888888888");
 
 CREATE TABLE groups (
 groupID INTEGER(11) AUTO_INCREMENT NOT NULL,
