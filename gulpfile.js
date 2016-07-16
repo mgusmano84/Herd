0d2438f4056c84b4ebc8b6284bf007915e52cb36
maryhoag <marymcevoyhoag@gmail.com>
2016-07-15 22:21:13 -0400

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/*this file is required for Sass compile via Gulp*/
+
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bourbon = require('node-bourbon');
+//
+//test branch to see that gulp actually works; test by running gulp in cli
+// gulp.task('default', function() {
+//   console.log('hi');
+// })
+
+ 
+gulp.task('sass', function () {
+	/*will search for and compile all Sass files*/
+  	gulp.src('assets/**/*.scss')
+
+    /*.pipe(sass().on('error', sass.logError))
+    /*converts Sass to CSS within gulp-sass*/
+    //.pipe(sass({
+    	//includePaths: require('node-bourbon').includePaths
+    	//includePaths: ['styles'].concat(neat)
+    
+    /*default css destination, Gulp will create this file*/
+    .pipe(gulp.dest('styles'));
+});
+ 
+gulp.task('sass:watch', function () {
+	/*listens to Sass*/
+  gulp.watch('assets/**/*.scss', ['sass']);
+});
+
+ 
