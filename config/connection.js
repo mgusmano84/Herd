896f326f0da287f89418e57b2de590c97d3f9eec
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-07-22 22:37:38 -0400

    heroku3

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ var password = process.argv[3];
 //creating the connection to the db
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+
 //sql local connection
 // mysql.createConnection({
 // 	host: 'localhost',
@@ -22,6 +23,12 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // if fail to connect display the error otherwise if successful give the connection id
 connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
 
 // 	function(err){
 
