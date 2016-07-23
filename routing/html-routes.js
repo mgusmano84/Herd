d28450a13cff3b79fb8ca91a383748625b386443
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-07-23 10:09:26 -0400

    heroku6

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -181,10 +181,12 @@ module.exports = function(app){
 
 	// this url lists the groups a user is currently in
 	app.get('/dashboard/yourgroups', function(req, res) {
+
+		console.log(req.user + "test");
+		console.log(res + "test");
 		
 		if(req.isAuthenticated()){
-			console.log(req.user + "test")
-			console.log(res + "test")
+			
 
 			// calls the orm that searches the database for all the groups you are in
 			orm.searchUserGroups(req.user, res);
